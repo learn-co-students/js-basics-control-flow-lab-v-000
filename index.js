15058be617707e69feb6f67334bdc0a6c96f3c7c
johnsontaiwo <josseytee@gmail.com>
2017-09-28 20:46:14 -0400

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function scuberGreetingForFeet(feet) {
  if (feet <= 2000) {
     return "This one is on me!"
   } else if (feet > 2000 && feet <= 2500) {
-    debugger
+    
     return 'I will gladly take your thirty bucks.'
   } else if (feet > 2500) {
     return 'No can do.'

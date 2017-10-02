6843f09b011310fa74d590374996ca78ff8425fc
halfields <hfields697@aol.com>
2017-10-02 01:19:37 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+let distance;
+let destination;
+let tip;
+let reply;
+
+function scuberGreetingForFeet(distance) {
+	if (distance <= 2000) {
+		return 'This one is on me!';
+	}
+	else if (distance > 2000 && distance <= 2500) {
+		return 'I will gladly take your thirty bucks.'
+	}
+	else {
+		return 'No can do.'
+	}
+}
+
+function ternaryCheckCity(destination) {
+	reply = (destination === "NYC") ?  "Ok, sounds good." : "No go.";
+	return reply;
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+  	case 'generous':
+  		return 'Thank you so much.'
+  	case 'not as generous':
+  		return 'Thank you.'
+  	default:
+  		return 'Bye.'
+  }
+}

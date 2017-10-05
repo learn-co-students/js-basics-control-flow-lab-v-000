284bdd0c80c321175cc3d95b4d30ff81732aeeb6
Mel Chang <mel510@users.noreply.github.com>
2017-10-04 22:27:25 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code in this file!
+
+let feet, city, tip;
+
+
+function scuberGreetingForFeet (feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  }
+  else if (feet > 1999 && feet < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'No can do.';
+  }
+}
+
+function ternaryCheckCity (city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip (tip) {
+  if (tip === 'generous') {
+    return 'Thank you so much.';
+  }
+  else if (tip === 'not as generous') {
+    return 'Thank you.';
+  }
+  else {
+    return 'Bye.';
+  }
+}

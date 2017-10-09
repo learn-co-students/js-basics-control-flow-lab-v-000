1473d07fdc4e08ba942393624abc299586781ab4
Bryan Hou <bryan.hou94@gmail.com>
2017-10-09 03:29:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your code in this file!
+function scuberGreetingForFeet(dist) {
+	if (dist <= 400) {
+		return 'This one is on me!';
+	} else if (dist >2500) {
+		return 'No can do.';
+	} else if (dist > 2000) {
+		return 'I will gladly take your thirty bucks.';
+	}
+
+}
+
+function ternaryCheckCity(city) {
+	switch (city) {
+		case 'NYC':
+			return "Ok, sounds good.";
+			break;
+		default:
+			return "No go.";
+			break;
+	}
+}
+
+function switchOnCharmFromTip(tip) {
+	switch (tip) {
+		case 'generous':
+			return 'Thank you so much.'
+			break;
+		case 'not as generous':
+			return 'Thank you.'
+			break;
+		default:
+			return 'Bye.'
+			break;
+	}
+}

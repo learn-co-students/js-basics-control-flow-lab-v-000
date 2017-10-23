1cb39e5a2e458558f545f981abaaf7f1358e8130
Araf Rahman <araf91@gmail.com>
2017-10-23 17:19:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance >= 2000 && distance < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
+}
+
+function ternaryCheckCity(city) {
+  let message;
+  city === "NYC" ? message = "Ok, sounds good." : message = "No go."
+  return message;
+}
+
+function switchOnCharmFromTip(tip) {
+  let message = '';
+  switch (tip) {
+
+    case 'generous':
+      message = "Thank you so much."
+      break;
+    case 'not as generous':
+      message = "Thank you."
+      break;
+    case 'thanks for everything':
+      message = "Bye."
+      break;
+  }
+  return message
+}

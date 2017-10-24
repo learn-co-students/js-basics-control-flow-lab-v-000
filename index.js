c46c39502a70f13d04d9579f81c7dbeb4b82f53d
veloblank <veloblank@gmail.com>
2017-10-24 19:34:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your code in this file!
+function scuberGreetingForFeet(distance){
+  if(distance > 2500) {
+    return "No can do.";
+  } else if(distance > 2000) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    if(distance <= 400) {
+      return "This one is on me!"
+    }
+  }
+}
+
+function ternaryCheckCity(destination){
+  let response;
+  destination === "NYC" ? response = "Ok, sounds good." : response = "No go.";
+  return response
+}
+
+function switchOnCharmFromTip(tip_amount){
+  switch(tip_amount){
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye."
+    }
+}

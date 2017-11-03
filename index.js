f049b6029db31ccf6f2273abd45e2f909c9fa02a
Sabrina Jones <sabrina.jones87@gmail.com>
2017-11-03 02:49:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your code in this file!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+    return 'This one is on me!'
+  }
+  else if (ride > 2000 && ride < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (ride >= 2500) {
+    return 'No can do.'
+  }
+}
+
+function ternaryCheckCity(city){
+  let destination = city
+  const message = (destination === "NYC") ? "Ok, sounds good." : "No go."
+  return message;
+}
+
+function switchOnCharmFromTip(tipType){
+ const tip = tipType
+ let response;
+
+ switch (tip){
+   case "generous":
+     response = 'Thank you so much.'
+     break;
+   case "not as generous":
+     response = 'Thank you.'
+     break;
+   default:
+     response = 'Bye.'
+     break;
+ }
+return response
+}

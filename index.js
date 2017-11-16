1a43ff4bd34bcc2cde291b8d1bcec9550ba9734e
Sophia H <shhubeny@gmail.com>
2017-11-16 06:19:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your code in this file!
+function scuberGreetingForFeet (scuber) {
+  if (scuber <= 400) {
+      return  "This one is on me!";
+    } else if (scuber > 2000 && 2500 > scuber) {
+      return  "I will gladly take your thirty bucks.";
+    } else {
+      return "No can do.";
+    } }
+
+function ternaryCheckCity (city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+}
+
+function switchOnCharmFromTip (tip) {
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      case 'not as generous':
+      return 'Thank you.';
+      default:
+      return 'Bye.';
+    }
+}

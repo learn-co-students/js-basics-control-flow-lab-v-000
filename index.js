ed833403f91a36700db4be9f07552184437d8906
Terry Blue <terryblue@comcast.net>
2017-11-28 19:55:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(distance) {
+
+  let response;
+
+    if (distance < 401) {
+      response = 'This one is on me!';
+    } else if (distance < 2501) {
+        response = 'I will gladly take your thirty bucks.';
+      } else {
+          response = 'No can do.';
+        }
+
+  return response;
+}
+
+function ternaryCheckCity(city) {
+
+  const response = (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
+
+  return response;
+}
+
+function switchOnCharmFromTip(tip) {
+
+  let response;
+
+  switch (tip) {
+
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+      break;
+  }
+
+  return response;
+}

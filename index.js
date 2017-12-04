183ee3577733aa018c7c23f57fb89f597be85175
jamesonbass1987 <jamesonbass@gmail.com>
2017-12-03 16:32:02 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(length) {
+
+  let response;
+
+  if (length <= 400) {
+    response = "This one is on me!";
+  } else if (length > 2000  && length < 2500) {
+    response = "I will gladly take your thirty bucks."
+  } else if (length > 2500) {
+    response = "No can do."
+  } else {
+    response = "Sorry, I don't know how to responde to that"
+  }
+
+  return response;
+}
+
+function ternaryCheckCity(city) {
+
+  let response;
+
+  city === 'NYC' ? response = "Ok, sounds good." : response = "No go."
+
+  return response
+}
+
+function switchOnCharmFromTip(tip) {
+
+  let response;
+
+  switch (tip) {
+    case "generous":
+      response = "Thank you so much.";
+      break;
+    case "not as generous":
+      response = "Thank you.";
+      break;
+    default:
+      response = "Bye."
+      break;
+    }
+
+  return response;
+}

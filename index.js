7ee985dea6a0eca8f5e39563c2d91e507f1f181e
alberto <github email address>
2017-12-13 15:25:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code in this file!
+function scuberGreetingForFeet (feet) {
+  if (feet <= 400){
+    return 'This one is on me!'
+  } else if (feet > 2500){
+    return 'No can do.'
+  } else if (feet > 2000){
+    return 'I will gladly take your thirty bucks.'
+  }
+}
+
+function ternaryCheckCity (city) {
+    let message
+    (city === 'NYC') ? message = 'Ok, sounds good.' : message = 'No go.'
+    return message
+}
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
+}

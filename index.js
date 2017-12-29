df2f203d45fa70b7d8b032d843e604824ec7f81b
Adam Tropp <adt6261@gmail.com>
2017-12-29 07:06:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+
+let message ;
+
+function scuberGreetingForFeet(ride) ;
+if (ride <= 400) {
+  message = 'This one is on me!'
+} else if (2000 < ride && ride < 2500) {
+  message = 'I will gladly take your thirty bucks.';
+} else if (ride > 2500) {
+  message = 'No can do.'
+}

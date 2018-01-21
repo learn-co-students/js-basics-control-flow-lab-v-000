651a69691470290cecc329dd738f4ab1c7c22ca1
lauraszyms <lauraszyms@gmail.com>
2018-01-20 23:15:22 -0800

    Done.

--- index.js
+++ index.js
@@ -20,11 +20,15 @@ function ternaryCheckCity(city) {
 }
 
 function switchOnCharmFromTip(tip) {
-  if (tip === "generous") {
-    return "Thank you so much."
-  } else if (tip === "not as generous") {
-    return "Thank you."
-  } else {
-    return "Bye."
-  }
+  switch (tip) {
+ case 'generous':
+   return "Thank you so much.";
+   break;
+ case 'not as generous':
+   return "Thank you.";
+   break;
+ default:
+    return "Bye.";
+      break;
+}
 }

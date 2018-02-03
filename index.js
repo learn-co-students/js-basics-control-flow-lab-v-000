ecf0fd3d35be968070b7bf85ee085d40e67f15fb
Christian Egglin <cegglin@conncoll.edu>
2018-02-03 20:19:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code in this file!
+function scuberGreetingForFeet (distanceToDestination) {
+  if (distanceToDestination <= 400) {
+    return "This one is on me!"
+  } else if (distanceToDestination > 2000 && distanceToDestination <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (distanceToDestination > 2500) {
+    return "No can do."
+  }
+}
+
+function ternaryCheckCity (destinationCity) {
+  return (destinationCity === 'NYC') ? "Ok, sounds good." : "No go.";
+}
+
+function switchOnCharmFromTip (tipQuality) {
+  switch (tipQuality) {
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    default:
+      return "Bye."
+  }
+}

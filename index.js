0151af93142894515481b6924fac83bc39eeddc0
btcline04 <btcline04@gmail.com>
2018-02-05 23:48:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code in this file!
+
+// let ride, city, tip, scuberGreetingForFeet, ternaryCheckCity, switchOnCharmFromTip;
+
+function scuberGreetingForFeet(ride) { 
+  if (ride <= 400) {
+    return "This one is on me!";
+  }
+    else if (ride > 2000) {
+      return "I will gladly take your thirty bucks.";
+    }
+      else if (ride > 2500) {
+        return "No can do.";
+      }
+};
+
+function ternaryCheckCity(city) {
+  city === "NYC" ? "Ok, sounds good." : "No go.";
+};
+
+// function switchOnCharmFromTip(tip) {
+//   return tip
+// }
+

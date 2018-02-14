e10fdd3beddf1af7cb60ed383a501ecb9e7f4ffb
Hannah Carnes <hecarnes@gmail.com>
2018-02-14 15:27:26 -0500

    first tests working

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code in this file!
+
+// gives customers a free sample if the ride is less than or equal to 400 feet
+
+// if the ride is >= 400 feet
+// return 'This one is on me!'
+// else 
+// nothing
+
+
+function scuberGreetingForFeet(rideLength) {
+  if (rideLength <= 400) {
+    return "This one is on me!";
+  } else if (rideLength >= 2000) {
+    return "I will gladly take your thirty bucks.";
+  } else if (rideLength >= 2500) {
+    return "No can do."
+  }
+}

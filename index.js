7be75483a287774caad3c2e746f4ca28b776b660
Morgan Fudge <mfudge92@gmail.com>
2018-03-01 15:12:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code in this file!
+function scuberGreetingForFeet(ride) {
+  if(ride <= 400) {
+    return 'This one is on me!'
+  }
+  if(ride > 2500) {
+    return 'No can do.'
+  }
+  if(ride > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
+}
+
+function ternaryCheckCity(c) {
+  return c === 'NYC' ? "Ok, sounds good." : "No go."
+}
+
+function switchOnCharmFromTip(t) {
+  switch(t) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
+}

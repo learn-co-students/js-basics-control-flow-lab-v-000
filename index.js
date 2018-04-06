6aa2ca19171e245241ca2328dc2a0c9a875ab954
blinded93 <blinded93@gmail.com>
2018-04-05 18:01:59 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return "This one is on me!"
+  } else if (distance > 2000 && distance < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (distance > 2500) {
+    return "No can do."
+  }
+}
+
+function ternaryCheckCity(city) {
+  return (city === "NYC" ? "Ok, sounds good." : "No go.");
+}
+
+function switchOnCharmFromTip(tip) {
+  let response;
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.'
+      break;
+  }
+  return response;
+}

30841476eb010648d055936038b40dfe9d8a6205
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-04-19 04:25:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet){
+  let result
+  if (feet <= 400){
+  result = 'This one is on me!';
+}
+if (feet > 2000){
+  result = 'I will gladly take your thirty bucks.';
+}
+if (feet > 2500){
+  result = 'No can do.';
+}
+return result
+}
+
+function ternaryCheckCity(city){
+  let result
+  if (city === 'NYC'){
+    result = 'Ok, sounds good.';
+  } else {
+    result = 'No go.';
+  }
+
+  return result
+}
+
+function switchOnCharmFromTip(tip){
+  let result
+  if (tip === 'generous'){
+    result = 'Thank you so much.';
+  }else if (tip === 'not as generous'){
+    result = 'Thank you.';
+  }else {
+    result = 'Bye.';
+  }
+
+
+  return result
+}

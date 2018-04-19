5bc38c8c34f4b4977b1aa95ab3dcfdcc6d21d990
Chris Henry <cdhenry@gmail.com>
2018-04-19 18:34:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let response;
+
+  if (someValue <= 400) {
+    response = 'This one is on me!';
+  } else if (someValue > 2000 && someValue < 2500) {
+    response = 'I will gladly take your thirty bucks.';
+  } else if (someValue > 2500) {
+    response = 'No can do.';
+  }
+  return response;
+}
+
+function ternaryCheckCity(someValue){
+  let response = (someValue === 'NYC') ? 'Ok, sounds good.' : 'No go.';
+
+  return response;
+}
+
+function switchOnCharmFromTip(someValue) {
+  let response;
+
+  switch (someValue) {
+    case "generous" :
+      response = 'Thank you so much.';
+      break;
+    case "not as generous" :
+      response = 'Thank you.';
+      break;
+    default :
+      response = 'Bye.';
+  }
+
+  return response;
+}

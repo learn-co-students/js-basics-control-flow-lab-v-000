6fc760b9c9f37daae008f29f411636231b6b4acc
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-04-20 11:39:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let message
+  if (someValue <= 400) {
+    message = 'This one is on me!';
+  } else if (someValue > 2000 && someValue < 2500){
+    message = 'I will gladly take your thirty bucks.';
+  } else if (someValue > 2500){
+    message = 'No can do.';
+  }
+  return message
+}
+
+function ternaryCheckCity(city) {
+  let message
+
+  if (city === 'NYC'){
+    message = 'Ok, sounds good.';
+  } else {
+    message = 'No go.'
+  }
+  return message
+}
+
+function switchOnCharmFromTip(tip) {
+  let message
+
+  if (tip === 'generous'){
+    message = 'Thank you so much.';
+  } else if (tip === 'not as generous'){
+    message = 'Thank you.';
+  } else {
+    message = 'Bye.';
+  }
+  return message
+}

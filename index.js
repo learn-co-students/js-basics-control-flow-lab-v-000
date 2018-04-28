1d9690e72f3eacea8c839cf6eca4fc2e17ecc733
raemillz <raeanne1991@gmail.com>
2018-04-28 16:07:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your code in this file!
+function scuberGreetingForFeet(ride) {
+  let comment;
+  if (ride <= 400) {
+    comment = 'This one is on me!';
+  }
+  else if (ride > 2000 && ride < 2500) {
+    comment = 'I will gladly take your thirty bucks.'
+  }
+  else if (ride > 2500) {
+    comment = 'No can do.'
+  }
+  return comment;
+}
+
+function ternaryCheckCity(city) {
+  let response;
+  if (city === 'NYC') {
+    response = "Ok, sounds good."
+  } else {
+    response = "No go."
+  }
+  return response;
+}
+
+function switchOnCharmFromTip(tip) {
+  let response
+  switch (tip) {
+    case 'generous':
+      response = "Thank you so much."
+      break;
+    case 'not as generous':
+      response = 'Thank you.'
+    break;
+    default:
+      response = "Bye."
+    break;
+  }
+  return response
+}

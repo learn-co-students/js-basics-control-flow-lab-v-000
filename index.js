8bd64f15397cccdc02822c5364badb5fa6c22b24
remclay <remmy.lauren@gmail.com>
2018-04-30 17:10:59 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let result
+  if (someValue <= 400) {
+    result = "This one is on me!";
+  } else if (someValue > 2000 && someValue < 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else if (someValue > 2500) {
+    result = "No can do.";
+  }
+return result;
+}
+
+function ternaryCheckCity(city) {
+  const result = (city === "NYC") ? "Ok, sounds good." : "No go.";
+  return result;
+}
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case "generous":
+      result = "Thank you so much.";
+      break;
+    case "not as generous":
+      result = "Thank you.";
+      break;
+    default:
+      result = "Bye.";
+  }
+  return result;
+}

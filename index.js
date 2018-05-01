53ed79eaee0227634f1a5d7b7aba6f9f347bdb7d
Eddie Melo <heriberto@melo.nyc>
2018-05-01 14:54:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400 ){
+    return 'This one is on me!';
+  } else if (feet > 2500){
+    return 'No can do.';
+  } else {
+    return 'I will gladly take your thirty bucks.';
+  }
+}
+function ternaryCheckCity(city){
+  if (city == 'NYC'){
+    return 'Ok, sounds good.';
+  } else{
+    return 'No go.';
+  }
+}
+function switchOnCharmFromTip(generous){
+  switch(generous){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
+}

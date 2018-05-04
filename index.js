1349cf92ce6e30030bde5eca361ff622206e1115
Man Leung <lepusleung@gmail.com>
2018-05-04 18:57:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+
+  let result;
+
+  if (feet <= 400) {
+    result = "This one is on me!";
+  } else if
+  (2000 < feet && feet < 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else if (feet >= 2501) {
+    result = "No can do.";
+  }
+  return result;
+}
+
+function ternaryCheckCity(location) {
+
+  let result;
+
+  location === "NYC" ? result = "Ok, sounds good." : result = "No go.";
+  return result;
+}
+
+function switchOnCharmFromTip(amt) {
+  switch (amt) {
+    case "generous":
+      return "Thank you so much.";
+      case "not as generous":
+      return "Thank you.";
+    default:
+    return "Bye.";
+  }
+}

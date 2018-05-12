1509f91193e1df6ca34308d4a93f3fbecf242f5f
ronald vilorio <soccer23415@gmail.com>
2018-05-12 17:11:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+    return "This one is on me!"
+  }else if (ride > 2500){
+    return "No can do."
+  }else if (ride > 2000){
+    return 'I will gladly take your thirty bucks.'
+  }
+}
+function ternaryCheckCity(city){
+  (city === 'NYC') ? 'Ok, sounds good.' : 'No go.'
+}

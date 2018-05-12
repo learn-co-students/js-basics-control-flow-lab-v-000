d67ed85ffeec8044695e191b652d6acccaee61fe
ronald vilorio <soccer23415@gmail.com>
2018-05-12 17:24:20 +0000

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,14 @@ function scuberGreetingForFeet(ride){
   }
 }
 function ternaryCheckCity(city){
-  (city === 'NYC') ? 'Ok, sounds good.' : 'No go.'
+  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.'
+}
+function switchOnCharmFromTip(tip){
+  if (tip === "generous"){
+    return 'Thank you so much.'
+  }else if(tip === 'not as generous'){
+    return 'Thank you.'
+  }else {
+    return 'Bye.'
+  }
 }

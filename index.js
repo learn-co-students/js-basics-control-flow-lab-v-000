890ae0c6b931f6347868c85a628aa6a37255fada
kpeck71 <kpeck71@gmail.com>
2018-05-14 18:00:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  let greeting
+
+  if (distance < 400) {
+    greeting = "This one is on me!"
+  }
+  else if (distance >= 2500) {
+    greeting = "No can do."
+  }
+  else if (distance > 2000 ) {
+    greeting = "I will gladly take your thirty bucks."
+  }
+  return greeting
+}
+
+function ternaryCheckCity(city) {
+  return (city == "NYC" ? "Ok, sounds good." : "No go.")
+}
+
+function switchOnCharmFromTip(tip) {
+  let farewell
+  switch (tip) {
+    case "generous":
+      farewell = "Thank you so much."
+      break;
+    case "not as generous":
+      farewell = "Thank you."
+      break;
+    default:
+      farewell = "Bye."
+  }
+  return farewell
+}

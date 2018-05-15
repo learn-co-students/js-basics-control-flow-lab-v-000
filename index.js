86acdcd8b90f5c76444eeaaaeb125d3993b46b63
TylerP33 <tyler3321@gmail.com>
2018-05-15 17:01:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your code in this file!
+let someValue;
+
+function scuberGreetingForFeet(someValue) {
+
+if (someValue <= 400) {
+	 return "This one is on me!";
+} else if (someValue > 2000 && someValue < 2499) {
+	 return "I will gladly take your thirty bucks.";
+} else if (someValue > 2500)
+	 return "No can do.";
+}
+
+
+
+
+
+
+function ternaryCheckCity(city) {
+let message
+city == "NYC" ? message =  "Ok, sounds good." : message = "No go.";
+return message 
+}
+
+
+function switchOnCharmFromTip (tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
+}

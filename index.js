d14b3bc629b9b186586b3c0aae38079d85c7738d
MyMac <mymac@JCB.local>
2018-05-18 12:29:14 -0700

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 // Write your code in this file!
-function scuberGreetingForFeet(someValue) {
-	if (someValue <= 400) {
-		return 'This one is on me!'
-	} else if (someValue > 2000 && someValue <= 2500) {
-		return 'I will gladly take your thirty bucks.'
+function scuberGreetingForFeet(feet) {
+	if (feet <= 400) {
+		return 'This one is on me!';
+	} else if (feet > 2000 && feet <= 2500) {
+		return 'I will gladly take your thirty bucks.';
 	} else {
-		return 'No can do.'
+		return 'No can do.';
 	}
 }
 
 function ternaryCheckCity(city) {
 	if (city === 'NYC') {
-		return 'Ok, sounds good.'
+		return 'Ok, sounds good.';
 	} else {
-		return 'No go.'
+		return 'No go.';
 	}
 }
 
 function switchOnCharmFromTip(tip) {
 	switch (tip) {
 		case 'generous':
-			return 'Thank you so much.'
+			return 'Thank you so much.';
 			break 
 		case 'not as generous':
-			return 'Thank you.'
+			return 'Thank you.';
 			break	
 		default:
-			return 'Bye.'
+			return 'Bye.';
 	}
 
 }

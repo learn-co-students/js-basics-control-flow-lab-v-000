5b8f38ed1a083952325632ebbaa472a91517a8bc
Salma Elshaelshahawy <salma71@gmail.com>
2018-05-20 11:25:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(distance) {
+
+    if (distance <= 400) {
+        return "This one is on me!";
+    } else if (distance > 2000 && distance < 2499) {
+        return "I will gladly take your thirty bucks.";
+    } else if (distance > 2500)
+        return "No can do.";
+}
+
+function ternaryCheckCity(city) {
+    let message;
+    city === "NYC" ? message = "Ok, sounds good." : message = "No go.";
+    return message;
+}
+
+function switchOnCharmFromTip(tip) {
+    switch (tip) {
+        case "generous":
+            return "Thank you so much.";
+        case "not as generous":
+            return "Thank you.";
+        default:
+            return "Bye.";
+    }
+}

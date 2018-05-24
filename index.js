6513076f015f893e5d4644ceda85c0773eeed817
AndrewHongUW <32497926+AndrewHongUW@users.noreply.github.com>
2018-05-24 15:21:05 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(distance) {
+  let greeting;
+  if (distance < 400) {
+    greeting = "This one is on me!";
+  }
+  else if (distance > 2000 && distance < 2500) {
+    greeting = "I will gladly take your thirty bucks."
+  }
+  else {
+    greeting = "No can do."
+  }
+  return greeting
+}
+
+function ternaryCheckCity(city) {
+  let message;
+  (city === "NYC") ? message = "Ok, sounds good." : message = "No go." ;
+  return message;
+}
+
+function switchOnCharmFromTip(tip) {
+  let message;
+  switch (tip) {
+    case 'generous':
+    message = "Thank you so much.";
+    break;
+    case 'not as generous':
+    message = "Thank you.";
+    break;
+    case 'thanks for everything':
+    message = "Bye.";
+    break;
+  }
+  return message;
+}

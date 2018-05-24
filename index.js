cc7c748f8e6e03db6809ff9ad75075a33760978f
github username <yosa.yon@gmail.com>
2018-05-24 10:42:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code in this file!
+function scuberGreetingForFeet(sampleFeet){
+  if (sampleFeet <= 400){
+    return "This one is on me!"
+  }
+  else if (sampleFeet > 2000 && sampleFeet < 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else{
+    return "No can do."
+  }
+}
+
+function ternaryCheckCity(city){
+ return (city === "NYC" ? "Ok, sounds good." : "No go.")
+}
+
+function switchOnCharmFromTip(tip){
+ switch (tip){
+  case 'generous':
+  return "Thank you so much."
+  case 'not as generous':
+  return "Thank you."
+  default:
+  return "Bye."
+ }
+}

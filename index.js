289915b9dcf8944c99ec539020e2754c188b77a0
codeesh <vineshpersaud@gmail.com>
2018-05-26 13:40:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+
+  let result
+  
+  if (someValue < 400){
+    result = 'This one is on me!' ;
+  }
+  else if (someValue > 2000 && someValue < 2500){
+    result = 'I will gladly take your thirty bucks.' ;
+  }
+  else if (someValue > 2500){
+    result = 'No can do.';
+  }
+  return result
+}
+
+function ternaryCheckCity(someValue){
+  let result
+  someValue === "NYC" ? result = "Ok, sounds good."  : result = "No go." ;
+  return result
+}
+
+function switchOnCharmFromTip(someValue){
+  let result
+  switch (someValue){
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      result = 'Thank you.';
+      break;
+    default: 
+      result = 'Bye.'
+      break;
+  }
+  return result
+}

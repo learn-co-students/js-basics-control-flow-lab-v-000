aee6cd586aba261f04fc44df99ab0e63c6ac35c6
Eden <emesfun@outlook.com>
2018-05-29 15:51:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(feet) {
+    let response; 
+
+    if (feet >= 0 && feet <= 400) {
+        response = 'This one is on me!';
+    }
+    else if (feet >= 2000 && feet <= 2499) {
+        response = 'I will gladly take your thirty bucks.';
+    }
+    else if (feet >= 2500) {
+        response = 'No can do.';
+    }
+
+    return response;
+}
+
+function ternaryCheckCity(destination) {
+    let response; 
+
+    if (destination == 'NYC') {
+        response = 'Ok, sounds good.'
+    }
+    else if (destination != 'NYC') {
+        response = 'No go.'
+    }
+
+    return response;
+}
+
+function switchOnCharmFromTip(tip) {
+    let response;   
+
+    switch(tip) {
+        case 'generous':
+            response = 'Thank you so much.';
+            break;
+        case 'not as generous':
+            response = 'Thank you.';
+            break;
+        default:
+            response = 'Bye.';
+            break; 
+    }
+    
+    return response;
+}

001f57a0f847d9abee17920ba54b81d6ebd119fc
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-06-12 15:12:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet (feet) {
+  let greeting;
+  if (feet <= 400) {
+    greeting = "This one is on me!";
+  } else if (feet > 2000) {
+    greeting = "I will gladly take your thirty bucks.";
+    if (feet >= 2500) {
+      greeting = "No can do.";
+    }
+  }
+  return greeting;
+}
+
+function ternaryCheckCity (city) {
+  let message;
+  city === 'NYC' ? message = "Ok, sounds good." : message = "No go."
+  return message;
+}
+
+function switchOnCharmFromTip (tip) {
+  let charm;
+  switch (tip) {
+    case 'generous':
+      charm = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      charm = 'Thank you.';
+      break;
+    default:
+      charm = 'Bye.';
+  }
+  return charm;
+}

b2cc3c7d43ce458262c32395ead155b9b7cf6faa
johnwojo <jwojo13@gmail.com>
2018-06-14 13:08:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+
+  if (someValue < 400) {
+    return 'This one is on me!';
+}
+
+  else if (someValue >= 400 && someValue < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+
+  else if (someValue > 2500) {
+    return 'No can do.'
+  }
+}
+
+
+function ternaryCheckCity(someCity) {
+  let response
+  response = someCity === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  return response
+}
+
+
+
+function switchOnCharmFromTip(someTip) {
+  if (someTip === 'generous') {
+    return 'Thank you so much.';
+  }
+
+  else if (someTip === 'not as generous') {
+    return 'Thank you.';
+  }
+
+  else {
+    return 'Bye.';
+  }
+}

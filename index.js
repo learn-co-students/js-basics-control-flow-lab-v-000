8d766c9278930b94be32106c3aa9a24de4916407
Michael Hirschberger <mlhirschberger@gmail.com>
2018-06-25 15:33:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(distance) {
+    let greeting;
+
+    if (distance <= 400) {
+        greeting = "This one is on me!";
+    } else if (distance > 2500) {
+        greeting = "No can do.";
+    } else if (distance > 2000) {
+        greeting = "I will gladly take your thirty bucks.";
+    }
+
+    return greeting;
+}
+
+function ternaryCheckCity(city) {
+
+    const response = (city === 'NYC') ? "Ok, sounds good." : "No go.";
+
+    return response;
+}
+
+function switchOnCharmFromTip(tip) {
+
+    let response;
+
+    switch(tip) {
+        
+        case 'generous':
+            response = "Thank you so much.";
+            break;
+
+        case 'not as generous':
+            response = "Thank you.";
+            break;
+
+        default:
+            response = "Bye.";
+            break;
+    }
+
+    return response;
+}

455e6a6d1ac14df4276ee073cf78265301d196ec
daniellecroxton <danikrist@gmail.com>
2018-07-06 09:56:07 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let greeting
+    if (someValue < 400) {
+      greeting = 'This one is on me!';
+    } else if (someValue > 2500) {
+      greeting = 'No can do.';
+    } else if (someValue > 2000) {
+      greeting = 'I will gladly take your thirty bucks.';
+    }
+}
+
+function ternaryCheckCity() {
+
+}
+
+function switchOnCharmFromTip() {
+
+}

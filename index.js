d92c02fec2e24295d9f906950a4e3feb66be9f15
AubreyNeimeier <neimeier.1@osu.edu>
2018-07-16 08:32:05 -0700

    greetingforfeet draft complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+    let greeting;
+    if (feet <= 400) {
+        greeting = "This one is on me!";
+    } else if (feet < 2500) {
+        greeting = "I will gladly take your thirty bucks.";
+    } else {
+        greeting = "No can do.";
+    }
+
+    return greeting;
+}

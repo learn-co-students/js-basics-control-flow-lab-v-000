30d893aae117ea2daec907741c3d86456ba64546
mmelmer <mikemelmer@gmail.com>
2018-07-19 19:45:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let value
+  if (someValue < 400) {
+    value = "This one is on me!"
+  } else if (someValue) > 2500 {
+    value = "No can do."
+  } else {
+    value = "I will gladly take your thirty bucks."
+  }
+  return value
+}
+
+function ternaryCheckCity() {
+
+}
+
+function switchOnCharmFromTip() {
+
+}

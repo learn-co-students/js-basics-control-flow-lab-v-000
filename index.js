3f93297bdc6d0ce9c25c879a36301c005369e6ff
mmelmer <mikemelmer@gmail.com>
2018-07-19 20:08:12 -0400

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function scuberGreetingForFeet(someValue) {
   let value
   if (someValue < 400) {
     value = "This one is on me!"
-  } else if (someValue) > 2500 {
+  } else if (someValue > 2500) {
     value = "No can do."
   } else {
     value = "I will gladly take your thirty bucks."
@@ -10,10 +10,24 @@ function scuberGreetingForFeet(someValue) {
   return value
 }
 
-function ternaryCheckCity() {
-
+function ternaryCheckCity(city) {
+  let result
+  (city == "NYC") ? result = "Ok, sounds good." : result = "No go."
+  return result
 }
 
-function switchOnCharmFromTip() {
-
+function switchOnCharmFromTip(tip) {
+  let response
+  switch (tip) {
+    case 'generous':
+      response = "Thank you so much."
+      break;
+    case 'not as generous':
+      response = "Thank you."
+      break;
+    default:
+      response = "Bye."
+      break;
+  }
+  return response;
 }

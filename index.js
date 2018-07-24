277d71fdad6862cf0cf4de92a675535b3e2e7639
Naomi Dennis <njdennis94@gmail.com>
2018-07-24 16:24:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your code in this file!
+'use strict'
+function scuberGreetingForFeet(length){
+  if(length <= 400 )
+  {
+    return 'This one is on me!'
+  }
+  else if (length > 2000 && length < 2500)
+  {
+     return 'I will gladly take your thirty bucks.'
+  }
+  else
+  {
+    return 'No can do.'
+  }
+
+}
+
+function ternaryCheckCity(city){
+  if(city == 'NYC'){
+    return 'Ok, sounds good.'
+  }
+  else{
+    return 'No go.'
+  }
+}
+
+function switchOnCharmFromTip(tip){
+  if(tip == 'generous'){
+    return 'Thank you so much.'
+  }
+  else if(tip == 'not as generous'){
+    return 'Thank you.'
+  }
+  else if(tip == 'thanks for everything'){
+    return 'Bye.'
+  }
+}

aa71e54aa2c19ddcf4193cc0180e146d3809d3d5
cbshields <cbshields@outlook.com>
2018-07-25 14:54:11 -0700

    added solution as alternative

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 // Write your code in this file!
+/*solution - didn't need to set the result to a variable
 
+function scuberGreetingForFeet (feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (1999 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+}
+
+function ternaryCheckCity (city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip (tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
+
+*/
 function scuberGreetingForFeet(someValue) {
     let result;
     if (someValue <= 400) {

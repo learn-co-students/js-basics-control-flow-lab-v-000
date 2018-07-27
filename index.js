0994feec094e9cd2c070532cbe456ee2af91887f
Araba <araba.taylor20@gmail.com>
2018-07-27 08:52:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(x) {
+  let result
+
+if (x>2500){
+  result = "No can do."
+}else if (x <= 400){
+  result = "This one is on me!"
+}else if (x > 2000){
+  result = "I will gladly take your thirty bucks."
+}
+  return result
+}
+
+function ternaryCheckCity(location) {
+  return location === "NYC"?"Ok, sounds good.":"No go."
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+  case 'generous':
+    return "Thank you so much.";
+    break;
+  case 'not as generous':
+    return "Thank you.";
+    break;
+  default:
+    return  "Bye.";
+    break;
+  }
+}

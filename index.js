0f15bc164af8584dc73aa523e0fb299f4712ed19
Emi Ko <emi8emi@gmail.com>
2018-07-30 14:44:00 -0700

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-function scuberGreetingForFeet(someValue) {
-  if (someValue <= 400) {
-  return 'This one is on me!';
-}
-
-  else if (someValue > 2500) {
-    return 'No can do.';
-}
-  else if (someValue > 2000) {
-  return 'I will gladly take your thirty bucks.';
-}
+function scuberGreetingForFeet (feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (1999 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
 function ternaryCheckCity(city) {
@@ -19,10 +16,8 @@ function switchOnCharmFromTip(tip) {
   switch (tip) {
     case 'generous':
       return "Thank you so much.";
-       break;
     case 'not as generous':
       return 'Thank you.';
-       break;
     default:
     return 'Bye.'
   }

f99648d4128d1d1a1e95c4783a185882816c1e02
Branden Mosley <branden.mosley1337@gmail.com>
2018-07-31 19:11:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code in this file!
+function scuberGreetingForFeet(num) {
+  let result
+  if (num < 400) {
+    result = "This one is on me!";
+  } else if (num > 2000 && num < 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else if (num > 2500) {
+    result = "No can do.";
+  }
+  return result
+}
+
+function ternaryCheckCity(city) {
+  return (city === 'NYC' ? "Ok, sounds good." : "No go.")
+}
+
+function switchOnCharmFromTip(tipAmount) {
+  let response
+  switch (tipAmount) {
+    case 'generous':
+      response = "Thank you so much."
+      break;
+    case 'not as generous':
+      response = "Thank you."
+      break;
+    default:
+      response = "Bye."
+      break;
+  }
+  return response
+}

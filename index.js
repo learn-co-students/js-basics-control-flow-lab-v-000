f33ef61a5b4d079b2fd0cff24ac88b290f48089b
ktamoguis <ktamoguis@gmail.com>
2018-08-04 22:06:22 +0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code in this file!
+
+
+function scuberGreetingForFeet(someNumber){
+  let message;
+  if (someNumber < 400){
+    message = 'This one is on me!';
+  } else if (someNumber > 2500) {
+    message = 'No can do.'
+  } else if (someNumber > 2000) {
+    message = 'I will gladly take your thirty bucks.';
+  }
+  return message;
+}
+
+function ternaryCheckCity(location){
+  let reply;
+  reply = location === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return reply;
+}
+
+function switchOnCharmFromTip(tip) {
+  let message;
+  switch (tip){
+    case 'generous':
+      message = 'Thank you so much.'
+      break;
+    case 'not as generous':
+      message = 'Thank you.'
+      break;
+    case 'thanks for everything':
+      message = 'Bye.'
+      break;
+  }
+  return message;
+
+}

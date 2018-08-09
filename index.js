665bb8a76335dc1793bcf9ce4431ea09849ba084
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-09 14:23:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+function scuberGreetingForFeet(length) {
+  if (length < 400) {
+    return 'This one is on me!';
+  } else if (length > 2000 && length < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
+}
+
+function ternaryCheckCity(city) {
+  switch(city) {
+    case `NYC`:
+      return 'Ok, sounds good.';
+      break;
+    default:
+      return 'No go.';
+  }
+}
+
+function switchOnCharmFromTip(phrase) {
+  switch(phrase) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
+}

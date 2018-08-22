43d4e4b3e07c3f2fefcd7b664e2052a1cb95593e
github username <github email address>
2018-08-22 14:43:45 -0400

    saftey

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(ride) {
+  //this is where we can use conditionals given our argument, someValue
+  //don't forget to return whatever the result is!
+
+  let result
+if (ride < 400) {
+  result = "This one is on me!";
+} else if (ride > 2000 && ride < 2500) {result = 'I will gladly take your thirty bucks.'} else if (ride > 2500) {result = 'No can do.'}
+//at the end, if I want my function to return something, I need to state it:
+return result
+}

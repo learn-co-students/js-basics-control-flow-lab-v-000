7feabf0ad881bf3c665c26be9c8acf7f6e413f7f
github username <github email address>
2018-08-22 17:26:06 -0400

    done

--- index.js
+++ index.js
@@ -10,4 +10,30 @@ if (ride < 400) {
 } else if (ride > 2000 && ride < 2500) {result = 'I will gladly take your thirty bucks.'} else if (ride > 2500) {result = 'No can do.'}
 //at the end, if I want my function to return something, I need to state it:
 return result
+
+};
+
+function ternaryCheckCity(city) {
+  let answer;
+  city == "NYC" ? answer = 'Ok, sounds good.' : answer = "No go."
+  return answer
+};
+
+
+function switchOnCharmFromTip(tip) {
+  let answer;
+  switch(tip){
+  case 'generous':
+  answer = 'Thank you so much.';
+  break;
+  case 'not as generous':
+  answer = 'Thank you.'
+  break;
+  case 'not as generous':
+  answer = 'Thank you.'
+  break;
+  default:
+  answer = 'Bye.';
+  break;}
+  return answer
 }

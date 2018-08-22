c84ef9029d68079a446c6e8f15b7feab927b8db0
coryjbergquist <cory.j.bergquist@gmail.com>
2018-08-22 14:26:29 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  //this is where we can use conditionals given our argument, someValue
+  let message
+  if (someValue < 400) {
+    message = 'This one is on me!'
+  }
+  if (someValue > 2000) {
+    message = 'I will gladly take your thirty bucks.'
+  }
+  if (someValue > 2500) {
+    message = 'No can do.'
+  }
+  return message
+  //don't forget to return whatever the result is!
+}
+
+function ternaryCheckCity(city) {
+  let message;
+  city === "NYC" ? message = "Ok, sounds good." : message = "No go.";
+  return message
+}
+
+function switchOnCharmFromTip(tip) {
+  let message = "Bye.";
+
+  switch(tip) {
+    case 'generous':
+      message = "Thank you so much.";
+      break;
+    case 'not as generous':
+      message = "Thank you.";
+      break;
+  }
+  return message
+
+}

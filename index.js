53500de2dedd6a4ebbcec3b2b108573e6f5e5781
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-23 19:16:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(distance) {
+  
+  if(distance <= 400) {
+    return 'This one is on me!';
+  }
+  if(distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  if(distance > 2500) {
+    return 'No can do.';
+  }
+  
+}
+
+function ternaryCheckCity(city) {
+  let response;
+  city === "NYC" ? response = "Ok, sounds good." : response = "No go.";
+  return response;
+}
+
+function switchOnCharmFromTip(tip) {
+  let response;
+  switch(tip) {
+    case 'generous':
+      response = "Thank you so much.";
+      break;
+    case 'not as generous':
+      response = "Thank you.";
+      break;
+    default:
+      response = "Bye.";
+  }
+  return response;
+}
+

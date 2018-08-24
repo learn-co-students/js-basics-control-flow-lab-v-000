e2c3ced758b970f5580918b229aa7418fbe0c20b
arye-dov-eidelman <aryedov.e@gmail.com>
2018-08-24 17:27:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your code in this file!
+function scuberGreetingForFeet(distance){
+  if (distance < 400){
+    return 'This one is on me!';
+  }else if(distance > 2500){
+    return 'No can do.'
+  }else if(distance > 2000){
+    return 'I will gladly take your thirty bucks.';
+  }
+}
+
+function ternaryCheckCity(city){
+  if (city === 'NYC'){
+    return "Ok, sounds good."
+  }else{
+    return "No go.";
+  }
+}
+
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous'){
+    return  "Thank you so much.";
+  }else if(tip === 'not as generous'){
+    return "Thank you."
+  }else{
+    return 'Bye.';
+  }
+}

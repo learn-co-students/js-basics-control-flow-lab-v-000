bcb8d7a153124ad7aee3dbffbe008f51a2cf69c7
jonathan <jonathanyi81@gmail.com>
2018-09-04 20:35:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feetTraveled) {
+  let value
+  if (feetTraveled > 2500) {
+    value = "No can do."
+  } else if (feetTraveled > 2000){
+    value = "I will gladly take your thirty bucks."
+  } else if (feetTraveled <= 400){
+    value = "This one is on me!"
+  }
+  return value
+}
+
+function ternaryCheckCity(city) {
+  let value
+  city == "NYC" ? value = "Ok, sounds good." : value = "No go.";
+  return value;
+}
+
+function switchOnCharmFromTip(tip) {
+  let greeting = 'Bye.'
+  switch(tip){
+    case 'generous':
+      greeting = 'Thank you so much.'
+      break;
+    case 'not as generous':
+      greeting = "Thank you."
+      break;
+  }
+
+  return greeting
+}

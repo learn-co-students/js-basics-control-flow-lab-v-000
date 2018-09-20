eed10a4bd435e28f2666281cfc9b19c04b6f80ea
github username <github email address>
2018-09-20 13:42:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someValue) {
-  let result
   if (someValue <= 400) {
     return "This one is on me!";
   } else if (someValue > 2000 && someValue < 2500) {
@@ -10,17 +9,17 @@ function scuberGreetingForFeet(someValue) {
   }
 }
 
-function ternaryCheckCity(city) {
-  return (city === "NYC") ? "Ok, sounds good." : "No go.";
+function ternaryCheckCity(city){
+  return city === 'NYC'? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(tip) {
-  switch(tip) {
-    case "generous":
-      return "Thank you so much.";
-    case "not as generous":
-      return "Thank you.";
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
     default:
-      return "Bye.";
+      return 'Bye.';
   }
 }

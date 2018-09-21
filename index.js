0323e59d739f344bc8166be9a371d38ae03cdd79
alcesalces11331 <rollin.metzger@gmail.com>
2018-09-20 17:03:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(value) {
+    if (value <= 400) {
+        return "This one is on me!";
+    } else if (value > 2000 && value < 2500) {
+        return "I will gladly take your thirty bucks.";
+    } else {
+        return 'No can do.';
+    }
+};
+
+function ternaryCheckCity(value) {
+    if (value === "NYC") {
+        return "Ok, sounds good.";
+    } else {
+        return 'No go.';
+    }
+};
+
+function switchOnCharmFromTip(value) {
+    switch (value) {
+        case 'generous':
+        return 'Thank you so much.';
+        break;
+
+        case 'not as generous':
+        return 'Thank you.'
+        break;
+
+        case 'thanks for everything':
+        return 'Bye.'
+        break;
+    } 
+}
+

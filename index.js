57d75367848cb6ce873a8451796e77c74fea91dd
Brittany <britt.cumens@gmail.com>
2018-09-21 12:39:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  let result;
+  if feet <= 400 {
+    result = "This one is on me!"
+  } else {
+    result = "I will gladly take your thirty bucks."
+  };
+  return result
+};

da9d500f781fa71951cacf0657aee1e4b05e17bd
eyi1 <ej7729@gmail.com>
2018-10-10 19:56:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your code in this file!
+function scuberGreetingForFeet(number){
+    let result;
+    if (number <= 400){
+        return result = 'This one is on me!';
+    } else if (number > 2000 && number < 2500){
+        return result = 'I will gladly take your thirty bucks.';
+    }else{
+        return result = 'No can do.';
+    }
+}
+
+function ternaryCheckCity(city){
+
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
+}
+
+function switchOnCharmFromTip(value){
+    let msg;
+    switch (value){
+        case 'generous':
+            return msg = 'Thank you so much.';
+            break;
+        case 'not as generous':
+            return msg = 'Thank you.';
+            break;
+        default:
+            return msg = 'Bye.';
+            break;
+    }
+}

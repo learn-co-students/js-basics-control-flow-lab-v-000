fcd8394462756b0b4e7c6b913d451ce7bb735956
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-10-12 18:46:35 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your code in this file!
+function scuberGreetingForFeet(distanceInFeet) {
+  if (distanceInFeet > 2500) {
+      return "No can do.";
+  } else if (distanceInFeet > 2000) {
+      return "I will gladly take your thirty bucks.";
+  } else if (distanceInFeet <= 400) {
+      return "This one is on me!";
+  }
+}
+
+function ternaryCheckCity(city){
+  return (city === "NYC" ? "Ok, sounds good." : "No go.");
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip){
+    case "generous":
+        return "Thank you so much.";
+        break;
+    case "not as generous":
+        return "Thank you.";
+        break;
+    default:
+        return "Bye.";
+    }
+
+}

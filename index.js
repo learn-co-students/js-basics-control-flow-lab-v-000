bce5213cd9290d1f1e4b1100bdf5a33c76b6a63e
asabogal <asabogal.git@gmail.com>
2018-10-18 11:37:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(feet) {
+
+  let message
+    if (feet <= 400){
+      message = "This one is on me!"
+    } else if (1999 < feet && feet < 2500){
+      message = "I will gladly take your thirty bucks."
+    } else {
+      message = "No can do."
+    }
+
+  return message
+};
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+};
+
+
+function switchOnCharmFromTip(tip) {
+
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+        return "Bye."
+
+  }
+};

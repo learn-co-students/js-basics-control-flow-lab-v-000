28e9946612ec323db5dbff247b1db98b9d84ef63
anikonchuk <anikonch@gmail.com>
2018-10-26 11:16:05 -0400

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function scuberGreetingForFeet(distance) {
   let result
   if (distance < 400) {
     result = 'This one is on me!';
-  } else if (distance > 400 && distance < 2500){
+  } else if (distance > 1999 && distance < 2500){
     result = 'I will gladly take your thirty bucks.';
   } else {
     result = 'No can do.';

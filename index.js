55817319ad79a84da91c8a8b851138af192065d5
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-29 21:03:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  let result
+  if (distance < 400) {
+    result = 'This one is on me!';
+  } else if (distance > 1999 && distance < 2500){
+    result = 'I will gladly take your thirty bucks.';
+  } else {
+    result = 'No can do.';
+  }
+  return result
+}
+ function ternaryCheckCity(city) {
+  let result = city === "NYC" ? "Ok, sounds good." : "No go."
+  return result
+}
+ function switchOnCharmFromTip(tip) {
+  let result
+  switch (tip) {
+    case 'generous':
+      result = "Thank you so much.";
+      break;
+    case 'not as generous':
+      result = "Thank you.";
+      break;
+    default:
+      result = "Bye.";
+      break;
+  }
+  return result
+}

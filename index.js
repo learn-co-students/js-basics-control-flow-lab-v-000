95163c58da744986a49d81104dd53d3640224628
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-05 20:47:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let message
+  if (someValue > 0 && someValue <= 400) {
+    message = "This one is on me!"
+  }
+  else if (someValue > 2000 && someValue <= 2500) {
+    message = "I will gladly take your thirty bucks."
+  }
+  else if (someValue > 2500) {
+    message = "No can do."
+  }
+    
+  return message
+}
+
+function ternaryCheckCity(city) {
+  let message
+  message = city === "NYC" ? "Ok, sounds good." : "No go."
+  return message
+}
+
+function switchOnCharmFromTip(tip) {
+  let message
+  switch(tip) {
+    case "generous":
+      message = "Thank you so much."
+      break;
+    case "not as generous":
+      message = "Thank you."
+      break;
+    default:
+      message = "Bye."
+  }
+  return message
+}

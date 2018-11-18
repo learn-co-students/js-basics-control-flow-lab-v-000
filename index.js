26895432a834af7463b0a668666d67ec33cae4b8
epic0448 <samuelogundiran@Yahoo.com>
2018-11-17 19:32:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(feet) {
+   
+  if (feet > 2500){
+     return 'No can do.'
+  }
+  else if (feet > 2000 ) {
+      return 'I will gladly take your thirty bucks.'
+  }
+  else if (feet <= 400 ) {
+      return 'This one is on me!'
+  }
+}
+
+function ternaryCheckCity(city) {
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+   }
+
+   function switchOnCharmFromTip(tip) {
+       case tip = 'generous': 
+       response = 'Thank you so much.';
+       break
+
+       case tip != 'generous': 
+       response = 'Thank you.';
+       break
+       default:
+       response = 'Bye';
+   }

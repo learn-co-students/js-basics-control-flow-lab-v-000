9f1452c279ed089317ff8b85acd83ef3464faa25
epic0448 <samuelogundiran@Yahoo.com>
2018-11-19 16:59:52 -0500

    Done.

--- index.js
+++ index.js
@@ -17,17 +17,16 @@ function ternaryCheckCity(city) {
     return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
    }
 
-//    function switchOnCharmFromTip(tip)
-//      switch (tip) {
-//        case 'generous': 
-//        response = 'Thank you so much.';
-//        break;
-
-//        case tip != 'generous': 
-//        response = 'Thank you.';
-//        break;
+   function switchOnCharmFromTip(tip){
+    switch (tip) {
+        case 'generous': 
+        return 'Thank you so much.';
        
-//        default:
-//        response = 'Bye';
-//        break;
-//    }
+        case 'not as generous':
+         return 'Thank you.';
+
+        default:
+       return  'Bye.';
+       
+   }
+}

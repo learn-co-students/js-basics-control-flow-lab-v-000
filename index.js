f786483751592f2ce798dbd87a07542631a00cbe
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-20 04:00:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code in this file!
+
+
+function scuberGreetingForFeet(distance) {
+  let result;
+  if (distance <= 400) {
+    result = 'This one is on me!';
+  } else if (distance > 2000 && distance <= 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    result = 'No can do.';
+  }
+  return result;
+}
+
+function ternaryCheckCity(city) {
+  let result;
+  city === "NYC" ? result = "Ok, sounds good." : result = "No go.";
+  return result;
+}
+
+function switchOnCharmFromTip(tip) {
+  let result;
+  switch (tip) {
+    case 'generous':
+      result = "Thank you so much.";
+      break;
+    case 'not as generous':
+      result = "Thank you.";
+      break;
+    default:
+      result = "Bye.";
+  }
+  return result;
+  
+}

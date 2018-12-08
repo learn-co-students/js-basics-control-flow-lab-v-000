bbe2ebd2146d3eabd93cb6c66e3f74634d38e6f6
charlie  lee <ctlee9096@gmail.com>
2018-12-08 22:02:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
+// Write your code in this file!
 function scuberGreetingForFeet(someValue) {
-  let result
-  if (someValue <= 400) {
-    result = 'This one is on me!'
-  }
-  else if (someValue > 2500) {
-    result = 'No can do.'
+  let result;
+  if (someValue > 2500) {
+    result = "No can do."
   }
   else if (someValue > 2000) {
-    result = 'I will gladly take your thirty bucks.'
+    result = "I will gladly take your thirty bucks."
+  }
+  else if (someValue <= 400) {
+    result = "This one is on me!"
   }
   return result
 }
 
 function ternaryCheckCity(someCity) {
-  let result = (someCity === 'NYC') ? "Ok, sounds good." : "No go."
+  let result;
+  someCity == "NYC" ? result = "Ok, sounds good." : result = "No go."
   return result
 }
 
-function switchOnCharmFromTip(tip) {
-    switch (tip) {
-      case 'generous' :
-        return "Thank you so much."
-        break
-      case 'not as generous' :
-        return  "Thank you."
-        break
-      default :
-        return "Bye." 
+function switchOnCharmFromTip(someCharm) {
+  let result;
+  switch (someCharm) {
+    case 'generous':
+    result = "Thank you so much."
+    break;
+    case 'not as generous':
+    result = "Thank you."
+    break;
+    default:
+    result = "Bye."
+    break;
   }
+  return result
 }

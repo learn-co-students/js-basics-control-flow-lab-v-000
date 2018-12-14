033069d7c96df0718c5259fdd122b41ad3c579f4
BrittanyTinnin <bntinnin7@gmail.com>
2018-12-13 19:30:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(someValue) { 
+  let result;
+  if (someValue <= 400) {
+    result = 'This one is on me!'
+  } else if (someValue > 2000 && someValue < 2500) {
+    result = 'I will gladly take your thirty bucks.'
+  } else {
+    result = 'No can do.'
+  }
+  return result
+}
+
+function ternaryCheckCity(someCity) {
+    return someCity === 'NYC' ? "Ok, sounds good." : "No go."
+}
+
+function switchOnCharmFromTip(tipAmount) {
+  switch(tipAmount) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."
+      break;
+  }
+}

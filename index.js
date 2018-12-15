0417136efc38a56342714ed153fa045a143e8774
Madeline-Stark <madeline.stark1@gmail.com>
2018-12-14 17:38:16 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your code in this file!
+let rideDistance;
+
+function scuberGreetingForFeet(rideDistance) {
+  let result
+  if (rideDistance <= 400) {
+    result = 'This one is on me!';
+  }
+  else if (2000 < rideDistance && rideDistance < 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  else {
+    result = 'No can do.';
+  }
+
+  return result
+}
+
+let destination;
+
+function ternaryCheckCity(destination) {
+  return destination === "NYC" ? 'Ok, sounds good.' : 'No go.';
+}
+
+let tip;
+
+function switchOnCharmFromTip(tip) {
+  let result
+
+  switch(tip){
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      result = 'Thank you.';
+      break;
+    default:
+      result = 'Bye.';
+      break;
+  }
+  return result
+}

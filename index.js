28a467427e21a0b06c18995ad73fe9364c386898
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-26 17:34:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
 function scuberGreetingForFeet(distance) {
-  let message;
   if (distance <= 400) {
-    message = "This one is on me!";
+    return "This one is on me!";
   } else if (distance > 2000 && distance <= 2500) {
-    message = "I will gladly take your thirty bucks.";
+    return "I will gladly take your thirty bucks.";
   } else if (distance > 2500) {
-    message = "No can do.";
+    return "No can do.";
   }
-  return message;
 }
 
 function ternaryCheckCity(city) {
   return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(tip) {
-  let message;
+
+function switchOnCharmFromTip (tip) {
   switch (tip) {
     case "generous":
-      message = "Thank you so much.";
-      break;
+      return "Thank you so much.';
     case "not as generous":
-      message = "Thank you.";
-      break;
-    case "thanks for everything":
-      message = "Bye.";
-      break;
+      return "Thank you.";
+    default:
+      return "Bye.";
     }
-  return message;
-}
+  }

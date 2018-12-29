b01d16c47981b42998e779831032600c53c8a3f2
ryanwevans <ryanwevans@icloud.com>
2018-12-28 18:21:44 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let greeting;
+
+  if (someValue <= 400) {
+    greeting = "This one is on me!";
+  }
+
+  if (someValue > 2000) {
+    greeting = "I will gladly take your thirty bucks.";
+  }
+
+  if (someValue > 2500) {
+    greeting = "No can do.";
+  }
+
+  return greeting;
+}
+
+
+function ternaryCheckCity(city) {
+
+  return (city === 'NYC' ? "Ok, sounds good." : "No go.");
+
+}
+
+
+function switchOnCharmFromTip(tip) {
+  let response;
+
+  switch (tip) {
+    case 'generous':
+      response = "Thank you so much.";
+      break;
+    case 'not as generous':
+      response = "Thank you.";
+      break;
+    default:
+      response = "Bye.";
+      break;
+    }
+
+  return response;
+}

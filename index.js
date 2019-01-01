e59a8b49670c1bafa171cfc4052eab4abda1aa94
Emily Welsch <emwelsch@gmail.com>
2018-12-31 21:58:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your code in this file!
+function scuberGreetingForFeet(someValue){
+    let result
+    if (someValue <= 400){
+      result = "This one is on me!";
+    } else if (someValue > 2000 && someValue < 2500){
+      result = "I will gladly take your thirty bucks.";
+    } else{
+      result = "No can do."
+    }
+    return result
+}
+
+function ternaryCheckCity(city){
+  let result
+  if (city == "NYC"){
+    result = "Ok, sounds good.";
+  } else {
+    result = "No go."
+  }
+  return result
+}
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return "Bye."
+      break;
+  }
+}

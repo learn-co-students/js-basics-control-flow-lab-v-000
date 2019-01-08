fc7ebd5627fa3274c620be461b50e5819919d1ca
nichia <nichia@gmail.com>
2019-01-07 22:38:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    result = 'This one is on me!';
+  } else if (distance <= 2000) {
+    result = ' ';
+  } else if (distance <= 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  } else {
+    result = 'No can do.';
+  }
+  return result;
+}
+
+function ternaryCheckCity(city) {
+  city === 'NYC' ? (result = 'Ok, sounds good.') : (result = 'No go.');
+  return result;
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      result = 'Thank you.';
+      break;
+    default:
+      result = 'Bye.';
+      break;
+  }
+  return result;
+}

6cce1fdea01b0960c22fd348ef7a9f1a33c52043
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-01-08 16:25:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let result
+  if (someValue <= 400) {
+    result = 'This one is on me!';
+  } else if (1999 < someValue && someValue < 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  } else {
+    result = 'No can do.';
+  }
+  return result
+}
+
+function ternaryCheckCity(city) {
+  let result
+  if (city === 'NYC') {
+    result = 'Ok, sounds good.';
+  } else {
+    result = 'No go.';
+  }
+  return result
+}
+
+function switchOnCharmFromTip(tip) {
+  let result
+  if (tip === 'generous') {
+    result = 'Thank you so much.';
+  } else if (tip === 'not as generous') {
+    result = 'Thank you.';
+  } else {
+    result = 'Bye.';
+  }
+  return result
+}

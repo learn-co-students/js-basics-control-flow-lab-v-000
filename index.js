70f3e3b8d05cc7f8dea7b73567a3b0e0126fddde
David McGrann <dpmcgrann@gmail.com>
2019-01-08 21:02:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  let ride
+
+  if (distance <= 400) {
+    ride = 'This one is on me!';
+  } else if (distance >= 2000 && distance <= 2500) {
+    ride = 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    ride = 'No can do.';
+  }
+  return ride
+}
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+}
+
+function switchOnCharmFromTip(tip) {
+
+  switch (tip) {
+    case "generous" :
+      return "Thank you so much.";
+    case "not as generous" :
+      return "Thank you.";
+    default :
+      return "Bye.";
+  }
+
+}

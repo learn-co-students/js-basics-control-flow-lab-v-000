cea94875aaf9426417a2ca68f900600a6ba7eddc
lauradarlak <lauradarlak@gmail.com>
2019-01-16 12:03:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(someValue) {
+  let message
+  if (someValue < 400) {
+    message = 'This one is on me!';
+  } else if (someValue > 2500) {
+        message = 'No can do.';
+  } else if (someValue > 2000) {
+    message = 'I will gladly take your thirty bucks.';
+  }
+    return message;
+}
+
+function ternaryCheckCity(destination) {
+  const city = 'NYC';
+  const message = city === destination ? "Ok, sounds good." : "No go.";
+
+  return message;
+}
+
+function switchOnCharmFromTip(tip) {
+  let message;
+
+  switch (tip) {
+    case 'generous':
+      message = "Thank you so much.";
+      break;
+    case 'not as generous':
+      message = "Thank you.";
+      break;
+    default:
+      message = "Bye.";
+  }
+
+  return message;
+}

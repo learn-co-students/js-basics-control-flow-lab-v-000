5ec739a4fa88d3c278fc70a0b832eca8438cff76
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-01-30 19:10:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  if (feet < 400) {
+    result = 'This one is on me!';
+  } else if (feet > 2500) {
+    result = 'No can do.';
+  } else if (feet > 2000) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  return result
+}
+
+function ternaryCheckCity(city) {
+  let result;
+  city === 'NYC' ? (result = 'Ok, sounds good.') : (result = 'No go.');
+  return result
+}
+
+function switchOnCharmFromTip(tip) {
+  let result;
+  switch (tip) {
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      result = 'Thank you.';
+      break;
+    default:
+      result = 'Bye.';
+    }
+    return result;
+}

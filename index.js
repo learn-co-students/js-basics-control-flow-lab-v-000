45747a8e62795f14b1e61d335d42d9a86c958e57
github username <github email address>
2019-01-31 06:43:50 -0500

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,34 @@
 function scuberGreetingForFeet(distance) {
   let response;
   if (distance <= 400) {
-    response = 'This one is on me!'
+    response = 'This one is on me!';
   }
-  response
+  if (distance >= 400) {
+    response = 'I will gladly take your thirty bucks.';
+  }
+  if (distance > 2500) {
+    response = 'No can do.';
+  }
+  return response
+}
+
+function ternaryCheckCity(city) {
+  let response;
+  city === 'NYC' ? (response = 'Ok, sounds good.') : (response = 'No go.')
+  return response
+}
+
+function switchOnCharmFromTip(amount) {
+  switch (amount) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    case 'thanks for everything':
+      response = 'Bye.'
+      break;
+  }
+  return response
 }

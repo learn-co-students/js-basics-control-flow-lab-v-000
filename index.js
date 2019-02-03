51d894060e0f388a91a35b0bfe3dff4a31347885
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2019-02-03 22:31:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 function scuberGreetingForFeet(distance) {
   let response;
+
   if (distance <= 400) {
     response = "This one is on me!"
-  } else if (distance > 2000 && distance < 2500) {
-    response = "I will gladly take your thirty bucks."
-  } else if (distance > 2500) {
-    response = "No can do."
-  }
-  return response
+  } else if (distance > 2000) {
+    if (distance > 2500) {
+      response = "No can do."
+    } else {
+      response = "I will gladly take your thirty bucks."
+    }
+  };
+
+  return response;
 };
 
 function ternaryCheckCity(city) {
   let response;
   city === "NYC" ? response = "Ok, sounds good." : response = "No go."
-  return response
+
+  return response;
 };
 
 function switchOnCharmFromTip(tip) {
   let response;
-  switch (tip) {
-    case "generous" :
-      response = "Thank you so much.";
-      break;
-    case : "not as genorous"
-      response = "Thank you.";
-      break;
-    default :
-      response = "Bye.";
-      break;
+
+  switch(tip) {
+  case 'generous':
+    response = "Thank you so much."
+    break;
+  case 'not as generous':
+    response = "Thank you."
+    break;
+  default:
+    response = 'Bye.'
   };
+
   return response;
 };

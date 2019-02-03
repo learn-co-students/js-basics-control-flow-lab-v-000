9e358321ca345ef5c24cb1cd081eb4d3d25c5ed8
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2019-02-03 20:14:19 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function scuberGreetingForFeet(someValue) {
   let result;
   if (someValue <= 400) {
     result = "This one is on me!"
+  } else if (someValue > 2000 && someValue < 2500) {
+    result = "I will gladly take your thirty bucks."
+  } else if (someValue > 2500) {
+    result = "No can do."
   }
   return result
 }

ba601f6521c4fc4cadff5cc1b217f02f89b1dc04
Rebecca Strauss <rstrauss127@gmail.com>
2019-02-10 22:17:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  let message = "No can do.";
+
+  if (feet <= 400) {
+      message = "This one is on me!";
+  } else if (feet > 2000 && feet < 2501) {
+      message = 'I will gladly take your thirty bucks.';
+  }
+  return message
+}
+
+
+function ternaryCheckCity(city) {
+  city === "NYC" ? "Ok, sounds good." : "No go."
+}
+
+function switchOnCharmFromTip {
+  
+}

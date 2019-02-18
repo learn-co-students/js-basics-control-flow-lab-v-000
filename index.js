3c5fb555364532d25933e7765a35cbfad718fe73
Mohammed Noori <kmanoorib@gmail.com>
2019-02-18 04:11:11 +0000

    Done.

--- index.js
+++ index.js
@@ -17,13 +17,14 @@ function ternaryCheckCity(someCity) {
   }
 }
 
-function switchOnCharmFromTip(tip){
-  if (tip === "generous") {
-    return "Thank you so much.";
-  } else if (tip === "not as generous") {
-    return "Thank you.";
-  } else {
-    return "Bye.";
+function switchOnCharmFromTip (tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
   }
 }
 

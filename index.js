55bb35c198933efa82134bb67ea3e1b8157932cb
Mohammed Noori <kmanoorib@gmail.com>
2019-02-18 04:09:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someNumber) {
+  if (someNumber <= 400) {
+    return "This one is on me!";
+  } else if (someNumber > 2000 && someNumber < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+}
+
+function ternaryCheckCity(someCity) {
+  if (someCity === "NYC") {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
+}
+
+function switchOnCharmFromTip(tip){
+  if (tip === "generous") {
+    return "Thank you so much.";
+  } else if (tip === "not as generous") {
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
+}
+

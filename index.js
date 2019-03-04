11d2e27d11f567ea3f331ba11f806eaff67aca3a
zlehmann <zlehmann@gmail.com>
2019-03-04 14:13:13 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+    let greeting = "";
+
+    if (distance <= 400) {
+        greeting = "This one is on me!";
+    } else if (distance > 2000 && distance <=2500) {
+        greeting = "I will gladly take your thirty bucks.";
+    } else if (distance > 2500) {
+        greeting = "No can do.";
+    } else {
+        greeting = "I'm not sure how to respond.";
+    }
+    return greeting;
+}
+
+function ternaryCheckCity(destination) {
+    let response = "";
+
+    if (destination === "NYC") {
+        response = "Ok, sounds good.";
+    } else {
+        response = "No go.";
+    }
+    return response;
+}
+
+function switchOnCharmFromTip(tip) {
+    let response = "";
+
+    if (tip === 'generous') {
+        response = "Thank you so much.";
+    } else if (tip === 'not as generous') {
+        response = "Thank you.";
+    } else {
+        response = "Bye.";
+    }
+    return response;
+}

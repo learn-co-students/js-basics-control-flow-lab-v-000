3f1ed3943d7bb235b71352cd7207e57fe5988b48
nateshuster <nshu521992@gmail.com>
2019-03-16 12:00:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your code in this file!
+function scuberGreetingForFeet(dist) {
+  let result;
+
+  if (dist <= 400) {
+    result = 'This one is on me!';
+  } else if (dist > 2000 && dist < 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  } else if (dist > 2500) {
+    result = 'No can do.';
+  }
+
+  return result;
+}
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}

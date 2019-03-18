5ff681c90a2c67e211b73e5c2fc08cae74e9190c
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2019-03-18 17:39:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let response;
+  if (someValue <= 400) {
+    response = 'This one is on me!';
+  } else if (someValue > 2500) {
+    response = 'No can do.';
+  } else if (someValue > 2000) {
+    response = 'I will gladly take your thirty bucks.';
+  }
+  return response;
+};
+
+function ternaryCheckCity(someCity) {
+  let response;
+  someCity === 'NYC' ? response = 'Ok, sounds good.' : response = 'No go.';
+  return response
+};
+
+function switchOnCharmFromTip(someTip) {
+  let response;
+  switch(someTip) {
+    case 'generous':
+    response = "Thank you so much."
+    break;
+
+    case 'not as generous':
+    response = "Thank you."
+    break;
+
+    default:
+    response = "Bye."
+    break;
+  }
+  return response
+};

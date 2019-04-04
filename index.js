7e9286fd229e1578ae0f73a3a9bce4681cdb1c92
FionaDL <github email address>
2019-04-04 11:49:55 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code in this file!
+let someValue
+
+
+function scuberGreetingForFeet(someValue) {
+  let result
+if (someValue <= 400){
+  result = "This one is on me!"
+} else if(someValue >= 2000 && someValue < 2500) {
+  result = "I will gladly take your thirty bucks."
+}
+  else if (someValue > 2500) {
+    result = "No can do."
+  }
+  return result
+}
+
+let city
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+}
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much."
+      break;
+    case 'not as generous':
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+      break;
+
+  }
+}

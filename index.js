5c8c06a52ac7fd61ac0de9a568d3863e8a0950cf
corytveten <corytveten@gmail.com>
2019-04-07 09:01:15 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+    let message;
+    if (someValue <= 400) {
+        return message = 'This one is on me!'
+    } else if (someValue >= 2500) {
+        return message = 'No can do.'
+    } else {
+        return message = 'I will gladly take your thirty bucks.'
+    }
+}
+
+function ternaryCheckCity(city) {
+    let message;
+    city == "NYC" ? message = "Ok, sounds good." : message = "No go.";
+    return message;
+}
+
+function switchOnCharmFromTip(tip) {
+    let response;
+    switch (tip) {
+        case 'generous':
+            response = "Thank you so much.";
+            break;
+        case 'not as generous':
+            response =  "Thank you.";
+            break;
+        default:
+            response = "Bye.";
+    }
+    return response;
+}

89d141f389a81261ba3bd4b9c0ea91a1cacd58be
Michael Kang <michaelkang85@gmail.com>
2019-04-18 12:12:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your code in this file!
+function scuberGreetingForFeet(x){
+  let result
+  if (x < 400) {
+  result =  'This one is on me!'
+  }
+  else if (x > 2000 && x < 2500){
+    result =  'I will gladly take your thirty bucks.'
+  }
+  else {
+    (x > 2500)
+    result = 'No can do.'
+
+  }
+  return result
+}
+
+function ternaryCheckCity(x) {
+    let result
+    if (x == 'NYC'){
+      result = "Ok, sounds good."
+    }
+    else {
+      result = 'No go.'
+    }
+    return result
+}
+
+
+
+
+
+function switchOnCharmFromTip (tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}

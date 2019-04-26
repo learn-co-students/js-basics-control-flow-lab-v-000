53722db1d93124739dd2824fbebbbc71a94679ac
Taiwei Ko <mike25ko@gmail.com>
2019-04-26 16:22:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return "This one is on me!"
+  } else if ( distance > 2500){
+    return "No can do."
+  } else if (2000 < distance < 2500){
+    return "I will gladly take your thirty bucks."
+  } 
+}
+
+function ternaryCheckCity(city){
+  if (city === "NYC"){
+    return "Ok, sounds good."
+  } else{
+    return "No go."
+  }  
+}
+
+function switchOnCharmFromTip(tip){
+  
+let response;
+
+switch (tip) {
+    case 'generous':
+        response = 'Thank you so much.';
+        break;
+    case 'not as generous':
+        response = 'Thank you.';
+        break;
+    case 'thanks for everything':
+        response = 'Bye.';
+}
+
+return response;
+}
+

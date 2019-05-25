65c5f607df269f3c1499fdfa471046acf1b151dc
sarawall1187 <sarawall1187@gmail.com>
2019-05-25 08:38:02 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+    let ride;
+    if (someValue < 400) {
+        ride = 'This one is on me!';
+    }
+    else if (someValue > 2500) {
+        ride = 'No can do.';
+    }
+    else if (someValue > 2000){
+        ride = 'I will gladly take your thirty bucks.';
+    }
+    return ride
+  }
+
+  function ternaryCheckCity(someCity) {
+    let someCity = 'NYC'; 
+     someCity ? 'Ok, sounds good.':'No go.';
+   }
+
+ 

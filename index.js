cf691061002cbd5d6ef63b267fe49f1af558c3db
brennanmrose <brennan.m.rose@gmail.com>
2019-05-26 10:32:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function scuberGreetingForFeet(length_in_feet) {
-
 	let result
 	if (length_in_feet <= 400) {
 		result = 'This one is on me!'
@@ -8,35 +7,26 @@ function scuberGreetingForFeet(length_in_feet) {
 	} else if (length_in_feet > 2000) {
 		result = 'I will gladly take your thirty bucks.'
 	}
-
 	return result
-
 }
 
 function ternaryCheckCity(city) {
-
 	let result
 	if (city === 'NYC') {
 		result = 'Ok, sounds good.'
 	} else {
 		result = 'No go.'
 	}
-
 	return result
-
 }
 
-function switchOnCharmFromTip(tip) {
-
-	let result
-	if (tip === 'generous') {
-		result = 'Thank you so much.'
-	} else if (tip === 'not as generous') {
-		result = 'Thank you.'
-	} else {
-		result = 'Bye.'
-	}
-
-	return result 
-
+function switchOnCharmFromTip (tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }

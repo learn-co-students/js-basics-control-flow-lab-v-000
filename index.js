238e178db6c6ce62b568ce492ec85f9b832fe84e
Tadea <tsimunoviczg@gmail.com>
2019-05-30 13:41:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let result
+  if (someValue < 400) {
+    result = 'This one is on me!';
+  }
+  else if (someValue > 2000 && someValue < 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  else if (someValue > 2500) {
+    result = 'No can do.';
+  }
+  return result
+}
+
+function ternaryCheckCity(someCity) {
+  let result
+  return result = someCity === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function  switchOnCharmFromTip(someTip) {
+  switch (someTip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+      case 'not as generous':
+        return 'Thank you.';
+        break;
+    default:
+    return 'Bye.';
+
+  }
+}

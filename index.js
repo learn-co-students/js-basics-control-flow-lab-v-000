4a8e5e9e8c54aa3b5e01f836035b9e3bed9ee755
Brian Arango <brianvictus@gmail.com>
2019-06-04 22:27:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+function scuberGreetingForFeet(freeSample) {
+  let result;
+  if (freeSample <= 400) {
+    result = 'This one is on me!';
+  return result;
+  }
+
+function scuberGreetingForFeet(charge) {
+  let distance;
+  if (charge >=2000) {
+    distance = 'I will gladly take your thirty bucks.';
+}
+  return distance;
+}
+}

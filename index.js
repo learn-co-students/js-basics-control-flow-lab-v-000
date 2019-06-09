2353005fce81548c19093ef9ee264bd3bc1f3812
Benjamin Shapiro <bshap93@gmail.com>
2019-06-09 00:46:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400)
+    return 'This one is on me!';
+  else if (someValue > 2000 && someValue <= 2500)
+    return 'I will gladly take your thirty bucks.';
+  else if (someValue > 2500)
+    return 'No can do.';
+}
+
+function ternaryCheckCity(city) {
+  return ((city === 'NYC') ? 'Ok, sounds good.' : 'No go.');
+}
+
+function switchOnCharmFromTip(tip)
+{
+  if (tip === 'generous')
+    return 'Thank you so much.';
+  else if (tip === 'not as generous')
+    return 'Thank you.';
+  else {
+    return "Bye."
+  }
+}

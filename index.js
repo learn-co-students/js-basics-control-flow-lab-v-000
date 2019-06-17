59c87519e2ccd8c87dbcd6f8ec81206a756b3e07
Dan Hintz <dhintz@live.com>
2019-06-17 00:02:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  let result
+  if (distance <= 400) {
+    result = "This one is on me!";
+  }
+  else if (distance > 2000 && distance <= 2500) {
+    result = "I will gladly take your thirty bucks.";
+  }
+  else if (distance > 2500) {
+    result = "No can do.";
+  }
+  return result
+}
+
+function ternaryCheckCity(city) {
+  const result = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return result
+}
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case "generous":
+      return "Thank you so much.";
+      break
+    case "not as generous":
+      return "Thank you.";
+      break
+    default:
+      return "Bye.";
+  }
+}

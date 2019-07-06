5592b342bdad914082c56ad638758a44950b8deb
github username <github email address>
2019-07-06 15:29:58 -0400

    Done.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function scuberGreetingForFeet(someValue) {
       result = 'This one is on me!';
     } else if (someValue > 2000) {
       result = 'I will gladly take your thirty bucks.';
-    } else if (someValue > 2500) {
+    } else if (someValue < 2500) {
       result = 'No can do.';
     }
       return result

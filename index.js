6f104a70b85b150215f80c6472047478c5a73dca
corytveten <corytveten@gmail.com>
2019-07-06 09:33:10 -0700

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 // Write your code in this file!
-function scuberGreetingForFeet(someValue) {
-    let message;
-    if (someValue <= 400) {
-        return message = 'This one is on me!'
-    } else if (someValue >= 2500) {
-        return message = 'No can do.'
+function scuberGreetingForFeet(feet) {
+    if (feet <= 400) {
+        return "This one is on me!";
+    } else if (feet >= 2000 && feet < 2500) {
+        return "I will gladly take your thirty bucks.";
     } else {
-        return message = 'I will gladly take your thirty bucks.'
+        return 'No can do.'
     }
-}
+};
 
 function ternaryCheckCity(city) {
     let message;
-    city == "NYC" ? message = "Ok, sounds good." : message = "No go.";
+    city == "NYC" ? message = "Ok, sounds good." : message = "No go."
     return message;
 }
 
 function switchOnCharmFromTip(tip) {
-    let response;
+    let message;
     switch (tip) {
         case 'generous':
-            response = "Thank you so much.";
+            message = 'Thank you so much.';
             break;
-        case 'not as generous':
-            response =  "Thank you.";
+        case "not as generous":
+            message = "Thank you.";
             break;
         default:
-            response = "Bye.";
+            message = "Bye."
     }
-    return response;
+    return message;
 }

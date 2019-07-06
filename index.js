2616b6319ca32f2e7e9a9c6cf5a5d8ea96b03456
github username <github email address>
2019-07-06 15:12:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  //this is where we can use conditionals given our argument, someValue
+  //don't forget to return whatever the result is!
+    let result;
+    if (someValue < 400) {
+      result = 'This one is on me!';
+    } else if (someValue > 2000) {
+      result = 'I will gladly take your thirty bucks.';
+    } else if (someValue > 2500) {
+      result = 'No can do.';
+    }
+      return result
+
+}

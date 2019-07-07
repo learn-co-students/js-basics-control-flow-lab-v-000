5214466c8d95862f93d19f9caa6d62cf9a298daa
github username <github email address>
2019-07-06 20:13:55 -0400

    Done.

--- index.js
+++ index.js
@@ -5,11 +5,37 @@ function scuberGreetingForFeet(someValue) {
     let result;
     if (someValue < 400) {
       result = 'This one is on me!';
+    } else if (someValue >= 2500) {
+      result = 'No can do.';
     } else if (someValue > 2000) {
       result = 'I will gladly take your thirty bucks.';
-    } else if (someValue < 2500) {
-      result = 'No can do.';
-    }
-      return result
 
+    }
+    return result
+}
+
+function ternaryCheckCity(destination) {
+  let result;
+  if (destination === 'NYC') {
+    result = 'Ok, sounds good.';
+  } else {
+    result = 'No go.';
+  }
+  return result
+}
+
+function switchOnCharmFromTip(tip) {
+  let result;
+  switch (tip) {
+      case 'generous':
+          result = 'Thank you so much.';
+          break;
+      case 'not as generous':
+          result = 'Thank you.';
+          break;
+      default:
+          result = 'Bye.';
+          break;
+  }
+  return result;
 }

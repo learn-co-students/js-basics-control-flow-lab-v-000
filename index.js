689bc48daa5ef9e95e40fbd76619a9248d115ad1
Heshie Brody <4hbrody@gmail.com>
2019-07-08 22:39:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  let result;
+  if (feet <= 400) {
+    result = "This one is on me!";
+  } else if (feet > 2500) {
+    result = "No can do.";
+  } else {
+    result = "I will gladly take your thirty bucks.";
+  }
+  return result;
+}
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+}
+
+function switchOnCharmFromTip(tip) {
+  let response;
+
+  switch (tip) {
+    case "generous":
+      response = "Thank you so much.";
+      break;
+    case "not as generous":
+      response = "Thank you."
+      break;
+    default:
+    response = "Bye.";
+  }
+  return response;
+}

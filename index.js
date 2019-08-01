961420d07aabbcb31cf9d050635eead66020a6e1
Heather Miller <heather.miller114@gmail.com>
2019-08-01 16:16:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+function scuberGreetingForFeet (number) {
+    let response
+    if (number <= 400) {
+        response = 'This one is on me!';
+    } else if (number > 2000) {
+        response = 'I will gladly take your thirty bucks.';
+
+        if (number > 2500) {
+            response = 'No can do.';
+        }
+    }
+    return response
+}

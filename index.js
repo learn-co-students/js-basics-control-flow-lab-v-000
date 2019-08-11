7b03e1a8d670f584f8d2a630fb811a7250aaf246
github username <github email address>
2019-08-11 14:28:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(someValue) {
+    //this is where we can use conditionals given our argument, someValue
+    //don't forget to return whatever the result is!
+ 
+let result;
+ 
+if (someValue <= 400) {
+    result = 'This one is on me!';
+} else if (someValue < 2500) {
+    result = 'I will gladly take your thirty bucks.';
+} else if (someValue > 2500) {
+    result = 'No can do.';
+}
+return result;
+
+
+  }
+
+
+function ternaryCheckCity (city) {
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  }
+
+
+
+  function switchOnCharmFromTip (tip) {
+    switch (tip) {
+      case 'generous':
+        return 'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+      default:
+        return 'Bye.';
+    }
+  }

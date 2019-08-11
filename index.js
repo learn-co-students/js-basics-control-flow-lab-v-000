b8278324733be7db28e2d1498b8bf27fe8bb5eb5
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-08-11 16:26:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+function scuberGreetingForFeet(arg){
+ if (arg <= 400)
+  'This one is on me!';
+else if (arg > 2000 && arg <=2500) 
+  'I will gladly take your thirty bucks.';
+else
+  'No can do.';
+}
+
+function ternaryCheckCity(arg){
+  
+}
+
+function switchOnCharmFromTip(arg){
+  
+}

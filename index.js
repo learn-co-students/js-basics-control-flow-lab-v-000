dbe603e93e9cdcb1c8a908fb20bc112e926d7adf
Rebecca Tong <rtong@buffalo.edu>
2019-08-27 16:01:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  //this is where we can use conditionals given our argument, someValue
+  //don't forget to return whatever the result is!
+  let result
+  if (someValue <= 400) {
+    result = 'This one is on me!';
+  }
+  else if  (someValue >= 2500){
+   result = 'No can do.';
+ }
+
+  else if (someValue >= 2000) {
+     result = 'I will gladly take your thirty bucks.';
+   }
+  return result
+}
+
+
+function ternaryCheckCity(city) {
+ city === 'NYC' ? city = 'Ok, sounds good.' : city = "No go.";
+return city
+}
+
+
+function switchOnCharmFromTip(someValue){
+  let response;
+  switch (tip){
+    case 'generous':
+    response = 'Thank you so much.';
+    break;
+    case 'not as generous':
+    response = 'Thank you.';
+    break;
+    default:
+      response = "Bye.";
+      break;
+  }
+}

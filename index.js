d218ef23c35cffb5688ad340a464350d19f3eb12
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-09-03 03:00:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 // Write your code in this file!
 function scuberGreetingForFeet(arg){
- if (arg <= 400)
-  'This one is on me!';
-else if (arg > 2000 && arg <=2500) 
-  'I will gladly take your thirty bucks.';
-else
-  'No can do.';
+  if (arg <= 400){
+    return 'This one is on me!';
+  } else if (arg > 2500){
+      return 'No can do.';
+  } else if (arg > 2000) {
+      return 'I will gladly take your thirty bucks.';
+  }
 }
 
 function ternaryCheckCity(arg){
-  
+  var response = (arg === "NYC") ? "Ok, sounds good." : "No go.";
+  return response;
 }
 
 function switchOnCharmFromTip(arg){
-  
+  switch (arg) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default:
+    return 'Bye.'
+
+  }
 }

08721cddd23907ab8a7ccd8116bbc566b94e5da0
AlwinaO <aoyewole2586@gmail.com>
2019-09-05 00:49:15 +0100

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let ride;
+
+  if (distance <= 400) {
+    ride = "This one is on me!"
+  } else if (distance > 2000 || distance < 2500) {
+    ride = "I will gladly take your thirty bucks."
+  } else {
+    ride = "No can do."
+  }
+  return ride;
 }
 
 function ternaryCheckCity(){

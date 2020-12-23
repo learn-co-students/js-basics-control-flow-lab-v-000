dfdc49a6fa74d3b87e879721919785fc00c9b95f
John Dust <d24dust@hotmail.com>
2020-12-23 22:59:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  switch (true) {
+    case feet < 400:
+      return "This one is on me!";
+    case feet > 2500:
+      return "No can do.";
+    case feet > 2000:
+      return "I will gladly take your thirty bucks.";
+  }
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let rtn;
+  city === "NYC" ? rtn = "Ok, sounds good." : rtn = "No go."
+  return rtn
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }

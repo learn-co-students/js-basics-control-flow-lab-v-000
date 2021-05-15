6dd2dcf39f8dfdf23691abdbed32ff16acfb53fe
CharlesOK <okwechimeamechi@gmail.com>
2021-05-14 22:25:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  let response;
+  if (distance <= 400) {
+   response = "This one is on me!"
+
+  } else if (distance > 2500) {
+    response = "No can do."
+  } else {
+    response =  "I will gladly take your thirty bucks."
+  }
+   return response
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let response;
+  return response = (city === 'NYC') ? 'Ok, sounds good.' : 'No go.'
+ 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+   let gratitude;
+  switch (tip) {
+    case 'generous':
+      gratitude = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      gratitude = "Thank you.";
+      break;
+    case 'thanks for everything':
+      gratitude = 'Bye.';
+      break;
+    
+  }
+  return gratitude;
 }
